d0f1eac24b42eb312c3e66b8588ae84cbfd265d6
Richard Poate <info@zoople.com.au>
2025-05-04 10:52:13 +1000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace HTML_to_RTF_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.htmlEditControl1 = new Zoople.HTMLEditControl();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // htmlEditControl1
+            // 
+            this.htmlEditControl1.AcceptsReturn = true;
+            this.htmlEditControl1.BaseURL = null;
+            this.htmlEditControl1.CleanMSWordHTMLOnPaste = true;
+            this.htmlEditControl1.CodeEditor.Enabled = true;
+            this.htmlEditControl1.CodeEditor.Font = new System.Drawing.Font("Courier New", 10F);
+            this.htmlEditControl1.CodeEditor.Locked = false;
+            this.htmlEditControl1.CodeEditor.TabWidth = 720;
+            this.htmlEditControl1.CodeEditor.WordWrap = false;
+            this.htmlEditControl1.CSSText = null;
+            this.htmlEditControl1.DocumentHTML = null;
+            this.htmlEditControl1.EnableInlineSpelling = false;
+            this.htmlEditControl1.FontSizesList = null;
+            this.htmlEditControl1.FontsList = null;
+            this.htmlEditControl1.HiddenButtons = null;
+            this.htmlEditControl1.ImageStorageLocation = null;
+            this.htmlEditControl1.InCodeView = false;
+            this.htmlEditControl1.IndentAmount = 2;
+            this.htmlEditControl1.IndentsUseBlockuote = false;
+            this.htmlEditControl1.LanguageFile = null;
+            this.htmlEditControl1.LicenceActivationKey = null;
+            this.htmlEditControl1.LicenceKey = null;
+            this.htmlEditControl1.LicenceKeyInlineSpelling = null;
+            this.htmlEditControl1.Location = new System.Drawing.Point(18, 20);
+            this.htmlEditControl1.Name = "htmlEditControl1";
+            this.htmlEditControl1.Size = new System.Drawing.Size(709, 615);
+            this.htmlEditControl1.TabIndex = 0;
+            this.htmlEditControl1.ToolstripImageScalingSize = new System.Drawing.Size(16, 16);
+            this.htmlEditControl1.UseParagraphAsDefault = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(748, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(496, 613);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(461, 641);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "HTML to RTF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(748, 641);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(266, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "RTF to HTML";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 682);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.htmlEditControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Zoople.HTMLEditControl htmlEditControl1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HTML_to_RTF_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Rtf = ClipConvert.ConvertHtmlToRtf(this.htmlEditControl1.DocumentHTML);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClipConvert.ConvertRtfToHtml(this.richTextBox1.Rtf, ref this.htmlEditControl1);
+        }
+    }
+    public static class ClipConvert
+    {
+        public static string ConvertHtmlToRtf(string html)
+        {
+            var webBrowser = new WebBrowser { DocumentText = html };
+            while (webBrowser.DocumentText != html)
+                Application.DoEvents();
+
+            if (webBrowser.Document == null)
+                return null;
+
+            webBrowser.Document.ExecCommand("SelectAll", false, null);
+            webBrowser.Document.ExecCommand("Copy", false, null);
+
+            var richTextbox = new RichTextBox();
+            richTextbox.Paste();
+
+            return richTextbox.Rtf;
+        }
+        public static void ConvertRtfToHtml(string rtf, ref Zoople.HTMLEditControl Editor)
+        {
+            var rtfbox = new RichTextBox { Rtf = rtf };
+            rtfbox.SelectAll();
+            rtfbox.Copy();
+
+            Editor.SelectAll_Document();
+            Editor.Document.ExecCommand("paste", false, null);
+
+            return;
+        }
+    }
+}
+
+
